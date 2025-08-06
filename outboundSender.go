b90146bc7eaa71f47ffb70d9216402cd66efbd7e
ChaitanyaSingla <charlieksingla@gmail.com>
2025-08-06 16:51:49 +0200

    Increase waitTimeSeconds in sqs batching

--- outboundSender.go
+++ outboundSender.go
@@ -702,7 +702,7 @@ Loop:
 			consumedMessage, err := obs.sqsClient.ReceiveMessage(&sqs.ReceiveMessageInput{
 				QueueUrl:            aws.String(obs.sqsQueueURL),
 				MaxNumberOfMessages: aws.Int64(10),
-				WaitTimeSeconds:     aws.Int64(10),
+				WaitTimeSeconds:     aws.Int64(20),
 			})
 			if err != nil || len(consumedMessage.Messages) == 0 {
 				if err != nil {
